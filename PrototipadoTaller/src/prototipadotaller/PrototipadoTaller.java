5c6bac04aaed2325df21f56ad1c93079d060528e
Jdesparza <jdesparza@utpl.edu.ec>
2020-05-11 18:46:14 -0500

    Trabajo 4

---
+++ PrototipadoTaller/src/prototipadotaller/PrototipadoTaller.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototipadotaller;
+
+import java.util.Random;
+
+/**
+ *
+ * @author PC
+ */
+public class PrototipadoTaller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] values = new int[10];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = random.nextInt(10);
+        }
+
+        for (int i = 0; i < 10000000; i++) {
+            int index = -1;
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] == 80) {
+                    index = j;
+                }
+            }
+            break;
+        }
+    }
+    
+}
