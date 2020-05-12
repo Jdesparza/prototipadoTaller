e4646da1d170b0ba7db345829a78deb7ec4a915d
Jdesparza <jdesparza@utpl.edu.ec>
2020-05-11 19:56:34 -0500

    Taller

--- PrototipadoTaller/src/prototipadotaller/PrototipadoTaller.java
+++ PrototipadoTaller/src/prototipadotaller/PrototipadoTaller.java
@@ -24,10 +24,9 @@ public class PrototipadoTaller {
         }
 
         for (int i = 0; i < 10000000; i++) {
-            int index;
             for (int j = 0; j < values.length; j++) {
                 if (values[j] == 80) {
-                    index = j;
+                    int index = j;
                 }
             }
         }
