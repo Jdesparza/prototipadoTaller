55829fcd66332966603a064d2df4a174c4459ba6
Jdesparza <jdesparza@utpl.edu.ec>
2020-05-12 23:28:07 -0500

    Trabajo en clase

--- PrototipadoTaller/src/prototipadotaller/PrototipadoTaller.java
+++ PrototipadoTaller/src/prototipadotaller/PrototipadoTaller.java
@@ -21,13 +21,12 @@ public class PrototipadoTaller {
         }
 
         for (int i = 0; i < 10000000; i++) {
-            int index = -1;
             for (int j = 0; j < values.length; j++) {
                 if (values[j] == 80) {
-                    index = j;
-                    break;
+                    int index = j;
                 }
             }
+            break;
         }
     }
 }
